fbba55bf93f56224968b58f4bef6be7bcddc4aec
Nikita Sirovskiy <nicksirovsky@gmail.com>
2022-05-08 20:01:36 +0300

    Introduce MultiProvider

---
+++ packages/controllable_flutter/lib/src/x_multi_provider.dart
@@ -0,0 +1,22 @@
+import 'package:controllable_flutter/controllable_flutter.dart';
+import 'package:controllable_flutter/src/helpers/x_provider_single_child_widget_mixin.dart';
+import 'package:flutter/widgets.dart';
+
+class XMultiProvider<TController extends XController<TState, TEffect>,
+    TState extends XState, TEffect> extends MultiProvider {
+  XMultiProvider({
+    Key? key,
+    required List<XProviderSingleChildWidgetMixin> providers,
+    WidgetBuilder? builder,
+    Widget? child,
+  })  : assert(
+          (child != null && builder == null) ||
+              (child == null && builder != null),
+          'Either a child or a builder must be provided, not both at the same time',
+        ),
+        super(
+          key: key,
+          providers: providers,
+          child: builder != null ? Builder(builder: builder) : child,
+        );
+}
